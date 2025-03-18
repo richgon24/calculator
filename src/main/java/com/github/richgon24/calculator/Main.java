23870fdd5c01c8be4f4cdc9e21acd74480b3f2b4
Richard Gonzalez <rich_gonzalezz24@yahoo.com>
2025-03-18 01:54:55 -0700

    adds initial calc code

---
+++ src/main/java/com/github/richgon24/calculator/Main.java
@@ -0,0 +1,12 @@
+package com.github.richgon24.calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        calculator calculator = new calculator();
+
+        int sum = calculator.add(5, 3);
+        System.out.println("5 + 3 = " + sum);
+
+    }
+}
+
